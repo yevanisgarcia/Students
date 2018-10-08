dfe56a0eb364983c0c06ce5c925b2e8a9f907f0d
Yevanis Garcia <yevanis.garcia@gmail.com>
2018-10-07 22:54:59 -0400

    Initial commit

---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Students.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int StudentAge { get; set; }
+
+    }
+}
---
+++ Models/StudentContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Students.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+}
